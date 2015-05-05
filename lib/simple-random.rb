6b1009c5cbded5b44a529def2e9938cfa9b656d3
Krzysztof Kotlarski <kotlarski.krzysztof@gmail.com>
2015-05-05 14:15:25 +0200

    Simplify random instance thread safety; fixing race condition

--- lib/simple-random.rb
+++ lib/simple-random.rb
@@ -1,30 +1,7 @@
-require 'monitor'
-
 class SimpleRandom
-
   class << self
-
-    @instances = nil
-
     def instance
-
-      unless @instances
-        extend MonitorMixin
-
-        self.synchronize do
-          @instances ||= {}
-        end
-      end
-
-      instance_id = Thread.current.object_id
-
-      unless @instances[instance_id]
-        self.synchronize do
-          @instances[instance_id] ||= new
-        end
-      end
-
-      @instances[instance_id]
+      Thread.current[:simple_random_instance] ||= new
     end
   end
 
